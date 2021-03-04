3f6c8190d031102611dcb1e8b34b0c664f96e993
huydd29081999 <do.duc.huy@sun-asterisk.com>
2021-03-04 07:52:32 +0700

    init commit

---
+++ app/src/main/java/com/huydd2908/demomusic/SongActivity.kt
@@ -0,0 +1,11 @@
+package com.huydd2908.demomusic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song)
+    }
+}
